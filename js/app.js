aa085fd65ef9d1d0e2bfd93e1376379f6007ea70
Prashant <prashant@ginims.com>
2022-03-24 14:32:15 +0530

    local commit

---
+++ js/app.js
@@ -0,0 +1,88 @@
+let data = [
+  {
+    name: 'Bill',
+    job: 'CEO',
+    city: 'Seattle',
+    state: 'Washington',
+  },
+  {
+    name: 'Nazmus',
+    job: 'Software Engineer',
+    location: {
+      city: 'Boston',
+      state: 'Massachusetts',
+    },
+  },
+  {
+    name: 'Bob',
+    job: 'Engineer',
+    location: {
+      addres: {
+        state: 'New York',
+        city: 'New York City',
+        zip: '11323',
+      },
+    },
+  },
+  {
+    name: 'Chris',
+    job: 'Project Manager',
+    address: {
+      location: {
+        quadrant: {
+          block: 1,
+          state: 'Texas',
+          city: 'Austin',
+        },
+      },
+    },
+  },
+];
+
+const filterList = [
+  {
+    tittle: 'Name',
+    subItems: [],
+  },
+  {
+    tittle: 'City',
+    subItems: [],
+  },
+  {
+    tittle: 'State',
+    subItems: [],
+  },
+  {
+    tittle: 'Job',
+    subItems: [],
+  },
+];
+
+data.filter((item) => {
+  iterateObject(item);
+});
+
+function iterateObject(obj) {
+  for (prop in obj) {
+    if (typeof obj[prop] == 'object') {
+      iterateObject(obj[prop]);
+      //   console.log('--------------------------------------');
+    } else {
+      if (
+        prop === 'name' ||
+        prop === 'city' ||
+        prop === 'state' ||
+        prop === 'job'
+      ) {
+        // console.log(prop.toUpperCase() + ': ', obj[prop]);
+        filterList.forEach((list) => {
+          if (list['tittle'].toLowerCase() === prop) {
+            list.subItems.push(obj[prop]);
+          }
+        });
+      }
+    }
+  }
+}
+
+console.log(filterList);
